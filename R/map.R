c4795d47faca3c4c899229eaa56f89b3c1a909ae
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-08-09 10:57:54 -0400

    Style fussing, integrate interactivity on hover

--- R/map.R
+++ R/map.R
@@ -1,5 +1,5 @@
 map <- function() {
-  mapboxer::mapboxer(style = mapboxer::basemaps$Mapbox$streets_v11) %>%
+  mapboxer::mapboxer(style = "mapbox://styles/purposeanalytics/cl6mafpzk002r14pdbda7la8r") %>%
     mapboxer::set_view_state(-92, 52, zoom = 5) %>%
     mapboxer::add_navigation_control(showCompass = FALSE) %>%
     add_census_layer("ct") %>%
@@ -36,7 +36,8 @@ add_census_fill_layer <- function(map, geography) {
         ),
         layout = list(
           "visibility" = "none"
-        )
+        ),
+        popup = "{{geo_uid}}"
       )
     ) %>%
     mapboxer::add_layer(
@@ -46,7 +47,7 @@ add_census_fill_layer <- function(map, geography) {
         "source" = geography_to_source_id(geography),
         "source-layer" = geography_to_source_layer_id(geography),
         "paint" = list(
-          "fill-opacity" = 1,
+          "fill-opacity" = 0.75,
           "fill-color" = "white"
         ),
         layout = list(
@@ -62,20 +63,44 @@ add_census_fill_layer <- function(map, geography) {
 }
 
 add_census_line_layer <- function(map, geography) {
+  line_layer_id <- geography_to_layer_id(geography, "line")
+  hover_layer_id <- geography_to_layer_id(geography, "line_hover")
+
   map %>%
     mapboxer::add_layer(
       list(
-        "id" = geography_to_layer_id(geography, "line"),
+        "id" = line_layer_id,
         "type" = "line",
         "source" = geography_to_source_id(geography),
         "source-layer" = geography_to_source_layer_id(geography),
         paint = list(
-          "line-color" = "red",
+          "line-color" = "#0745a8",
           "line-width" = 1.5,
-          "line-opacity" = 0.5
+          "line-opacity" = 0.25
         ),
         layout = list(
           "visibility" = "none"
+        )
+      )
+    ) %>%
+    mapboxer::add_layer(
+      list(
+        "id" = hover_layer_id,
+        "type" = "line",
+        "source" = geography_to_source_id(geography),
+        "source-layer" = geography_to_source_layer_id(geography),
+        "paint" = list(
+          "line-color" = "#0745a8",
+          "line-opacity" = list(
+            "case",
+            list("boolean", c("feature-state", "hover"), FALSE), 1,
+            0
+          ),
+          "line-width" = list(
+            "case",
+            list("boolean", c("feature-state", "hover"), FALSE), 2,
+            1.5
+          )
         )
       )
     )
