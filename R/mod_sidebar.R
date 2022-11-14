58cd6753ca5c52e81f6555868016f2c669fc2345
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-11-14 14:02:23 -0500

    Exclude new inputs from bookmark

--- R/mod_sidebar.R
+++ R/mod_sidebar.R
@@ -113,7 +113,7 @@ mod_sidebar_server <- function(id, input_aggregate_area, input_selection_tool, s
 
     # Set up bookmarking ----
     bookmark_query <- shiny::reactive(
-      bookmark_query <- construct_bookmark(input, session, exclude = c("selection_tool", "export_data", "bookmark_selections", "export_geography", "export_boundary_bttn", "reset"), selected_geographies())
+      bookmark_query <- construct_bookmark(input, session, exclude = c("selection_tool", "export_data", "bookmark_selections", "export_geography", "export_boundary_bttn", "reset", "share", "download_report"), selected_geographies())
     )
     shiny::observeEvent(input$share, {
       shiny:::showBookmarkUrlModal(bookmark_query())
