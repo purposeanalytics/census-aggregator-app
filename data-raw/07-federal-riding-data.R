b7f7833ccd1b6c79af5230f3cab08a97a776bca1
daniel-simeone <109553256+daniel-simeone@users.noreply.github.com>
2025-03-24 10:53:05 -0500

    Revert "correct upload file"
    This reverts commit d90e26c3d1cb8b09d302122a3e3bb1dc4cd3f84b.

--- data-raw/07-federal-riding-data.R
+++ data-raw/07-federal-riding-data.R
@@ -539,12 +539,12 @@ if(FALSE){
 access_token <-   Sys.getenv('MAPBOX_SECRET_TOKEN')
 
 upload_tiles(
-  input = fed2023_upload,
+  input = test_upload,
   access_token = access_token,
   username = "purposeanalytics",
   tileset_id = "2021_ridings",
   tileset_name = "2021_census_ridings",
-  multipart=TRUE
+  multipart =FALSE
 )
 
 }
